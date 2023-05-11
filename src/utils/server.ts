1d04ac1654e33451170f01798711901c168c8984
Adr1 <adriangelmotto@gmail.com>
2023-05-11 14:49:37 +0200

    Fixing urls

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -7,19 +7,9 @@ export function createServer() {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
-  const allowedOrigins = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_LOCAL, process.env.MACHINE_IP];
-
-  const corsOptions = {
-    origin: function (origin: any, callback: any) {
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  };
-
-  app.use(cors(corsOptions));
+  app.use(cors({
+    origin: process.env.FRONTEND_URL_LOCAL + "*" || "http://localhost:3000",
+  }));
 
   return app;
 }
