504e22e70a95e352b109a769eeac9bdae928d400
Adr1 <adriangelmotto@gmail.com>
2023-05-12 15:24:41 +0200

    Reversed everything

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,20 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-require("dotenv").config();
-const CORS_ALLOWED_ORIGIN = process.env.CORS_ALLOWED_ORIGIN;
-console.log(CORS_ALLOWED_ORIGIN);
 
 export function createServer() {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
-  const corsOptions = {
-    origin: CORS_ALLOWED_ORIGIN,
-  };
-
-  app.use(cors(corsOptions));
+  app.use(cors());
 
   return app;
 }
